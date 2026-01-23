0ec482155973154093e8ea95a1e945b2bc3471a0
Lokesh S M <smlokesh2006@gmail.com>
2026-01-23 14:52:54 +0530

    improved normal recall and improved overall accuracy

--- src/no_img.py
+++
@@ -1,7 +0,0 @@
-import os
-
-test_normal = len(os.listdir("data/chest_xray/test/NORMAL"))
-test_pneumonia = len(os.listdir("data/chest_xray/test/PNEUMONIA"))
-
-print("Test NORMAL:", test_normal)
-print("Test PNEUMONIA:", test_pneumonia)
